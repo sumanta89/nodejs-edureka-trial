d777c1a9dc2780bc2216023497ab3f01860404af
Sumanta Kundu <sumantasccs@gmail.com>
2022-01-30 01:01:18 +0530

    add comment

--- node-server.js
+++ node-server.js
@@ -2,6 +2,8 @@
 
 //1. import http module
 const http = require("http");
+
+//create server on port 3100
 http.createServer((req, res)=>{
     res.end("Hello World from Node server");
 }).listen(3100);
